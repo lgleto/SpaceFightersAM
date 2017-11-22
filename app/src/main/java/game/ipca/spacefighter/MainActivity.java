1c7e343e7d1b703df9a6e9d8f0ac66b9f2b9f6e6
Lourenço Gomes <leto.lg@gmail.com>
2017-11-22 14:05:38 +0000

    first commit

---
+++ app/src/main/java/game/ipca/spacefighter/MainActivity.java
@@ -0,0 +1,13 @@
+package game.ipca.spacefighter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
